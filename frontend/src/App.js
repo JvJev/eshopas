3cd1e8cfcbbbac1b4b7ee012061c43d77c2f11af
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2022-10-31 15:15:04 +0200

    autoplius_commit1

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header classname="Main-header">
+        <a href="/" >autoplius kolnas?</a>
+      </header>
+      <main>
+        likes tinklapis
+      </main>
+    </div>
+  );
+}
+
+export default App;
