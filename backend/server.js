6fb4e0a743c3eaacfd91558f99efe3a14af5e89f
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2022-11-06 12:12:10 +0200

    mongoBD-and-product-schema-added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongo db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
@@ -11,7 +24,7 @@ app.get('/api/products/id/:id', (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: 'Product not found'})
+    res.status(404).send({ message: 'Product not found' });
   }
   res.send(data.products);
 });
